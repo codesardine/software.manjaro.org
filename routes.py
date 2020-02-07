bbfc07f2a3171bcf6fb04d3ec83cf1a87d831680
Vitor Lopes <vlopes@gmail.com>
2020-02-07 16:11:41 +0000

    refractor

--- routes.py
+++ routes.py
@@ -2,69 +2,68 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-def get_category(category):
+def get_categories():
+    return {'title': 'Featured', 'href': '/'},\
+           {'title': 'Photo & Video', 'href': 'photo_and_video'},\
+           {'title': 'Music & Audio', 'href': 'music_and_audio'},\
+           {'title': 'Productivity', 'href': 'productivity'},\
+           {'title': 'Communication & News', 'href': 'communication_and_news'},\
+           {'title': 'Games', 'href': 'games'},\
+           {'title': 'Education & Science', 'href': 'education_and_science'},\
+           {'title': 'Utilities', 'href': 'utilities'},\
+           {'title': 'Development', 'href': 'development'}
+
+
+def get_appstream_app_list(category):
     import pamac
     return pamac.Get().category(category)
 
 
 def template(category):
-    nav = {'title': 'Featured', 'href': '/'}, {'title': 'Photo & Video', 'href': 'photo_and_video'},\
-          {'title': 'Music & Audio', 'href': 'music_and_audio'}, {'title': 'Productivity', 'href': 'productivity'}, \
-          {'title': 'Communication & News', 'href': 'communication_and_news'}, {'title': 'Games', 'href': 'games'},\
-          {'title': 'Education & Science', 'href': 'education_and_science'}, \
-          {'title': 'Utilities', 'href': 'utilities'}, {'title': 'Development', 'href': 'development'}
-    return render_template("apps.html", apps=get_category(category), title=category, nav=nav)
+    return render_template("apps.html", apps=get_appstream_app_list(category), nav=get_categories())
 
 
 @app.route("/")
 def featured():
-    category = "Featured"
-    return template(category)
+    return template(get_categories()[0].get("title", ""))
 
 
 @app.route("/photo_and_video")
 def photo_and_video():
-    category = "Photo & Video"
-    return template(category)
+    return template(get_categories()[1].get("title", ""))
 
 
 @app.route("/music_and_audio")
 def music_and_audio():
-    category = "Music & Audio"
-    return template(category)
+    return template(get_categories()[2].get("title", ""))
 
 
 @app.route("/productivity")
 def productivity():
-    category = "Productivity"
-    return template(category)
+    return template(get_categories()[3].get("title", ""))
 
 
 @app.route("/communication_and_news")
 def communication_and_news():
-    category = "Communication & News"
-    return template(category)
+    return template(get_categories()[4].get("title", ""))
 
 
 @app.route("/education_and_science")
 def education_and_science():
-    category = "Education & Science"
-    return template(category)
+    return template(get_categories()[5].get("title", ""))
 
 
 @app.route("/games")
 def games():
-    category = "Games"
-    return template(category)
+    return template(get_categories()[6].get("title", ""))
 
 
 @app.route("/utilities")
 def utilities():
-    category = "Utilities"
-    return template(category)
+    return template(get_categories()[7].get("title", ""))
 
 
 @app.route("/development")
 def development():
-    category = "Development"
-    return template(category)
+    return template(get_categories()[8].get("title", ""))
+
