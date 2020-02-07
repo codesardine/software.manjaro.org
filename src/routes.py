a027a5676b00810a758269903af439a61ec4b3a4
Vitor Lopes <vlopes@gmail.com>
2020-02-07 19:01:33 +0000

    minor refractor, add missing title

--- src/routes.py
+++ src/routes.py
@@ -20,50 +20,50 @@ def get_appstream_app_list(category):
 
 
 def template(category):
-    return render_template("apps.html", apps=get_appstream_app_list(category), nav=get_categories())
+    return render_template("apps.html", apps=get_appstream_app_list(category), nav=get_categories(), title=category)
 
 
 @app.route("/")
 def featured():
-    return template(get_categories()[0].get("title", ""))
+    return template(get_categories()[0].get("title"))
 
 
 @app.route("/photo_and_video")
 def photo_and_video():
-    return template(get_categories()[1].get("title", ""))
+    return template(get_categories()[1].get("title"))
 
 
 @app.route("/music_and_audio")
 def music_and_audio():
-    return template(get_categories()[2].get("title", ""))
+    return template(get_categories()[2].get("title"))
 
 
 @app.route("/productivity")
 def productivity():
-    return template(get_categories()[3].get("title", ""))
+    return template(get_categories()[3].get("title"))
 
 
 @app.route("/communication_and_news")
 def communication_and_news():
-    return template(get_categories()[4].get("title", ""))
+    return template(get_categories()[4].get("title"))
 
 
 @app.route("/education_and_science")
 def education_and_science():
-    return template(get_categories()[5].get("title", ""))
+    return template(get_categories()[5].get("title"))
 
 
 @app.route("/games")
 def games():
-    return template(get_categories()[6].get("title", ""))
+    return template(get_categories()[6].get("title"))
 
 
 @app.route("/utilities")
 def utilities():
-    return template(get_categories()[7].get("title", ""))
+    return template(get_categories()[7].get("title"))
 
 
 @app.route("/development")
 def development():
-    return template(get_categories()[8].get("title", ""))
+    return template(get_categories()[8].get("title"))
 
