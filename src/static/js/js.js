10491ab5f569967f32f5026e446e34d8d472da16
Vitor Lopes <vlopes@gmail.com>
2020-02-11 16:46:16 +0000

    improve lazyloading, add package icon

--- src/static/js/js.js
+++ src/static/js/js.js
@@ -46,17 +46,37 @@ $('#search').keyup(debounce(function () {
 
 $(document).ready(function () {
   app = $("main div .app");
-  if ('loading' in HTMLImageElement.prototype) {
-    const images = document.querySelectorAll("img.lazyload");
-    images.forEach(img => {
-      img.src = img.dataset.src;
-    });
-  } else {
-    // Dynamically import the LazySizes library
-    let script = document.createElement("script");
-    script.async = true;
-    script.src =
-      "https://cdnjs.cloudflare.com/ajax/libs/lazysizes/4.1.8/lazysizes.min.js";
-    document.body.appendChild(script);
-  }
+  let lazyImages = [].slice.call(document.querySelectorAll("img.lazyload"));
+  let active = false;
+
+  const lazyLoad = function() {
+    if (active === false) {
+      active = true;
+
+      setTimeout(function() {
+        lazyImages.forEach(function(lazyImage) {
+          if ((lazyImage.getBoundingClientRect().top <= window.innerHeight && lazyImage.getBoundingClientRect().bottom >= 0) && getComputedStyle(lazyImage).display !== "none") {
+            lazyImage.src = lazyImage.dataset.src;
+            lazyImage.classList.remove("lazyload");
+
+            lazyImages = lazyImages.filter(function(image) {
+              return image !== lazyImage;
+            });
+
+            if (lazyImages.length === 0) {
+              document.removeEventListener("scroll", lazyLoad);
+              window.removeEventListener("resize", lazyLoad);
+              window.removeEventListener("orientationchange", lazyLoad);
+            }
+          }
+        });
+
+        active = false;
+      }, 200);
+    }
+  };
+
+  document.addEventListener("scroll", lazyLoad);
+  window.addEventListener("resize", lazyLoad);
+  window.addEventListener("orientationchange", lazyLoad);
 });
