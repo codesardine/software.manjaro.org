8b96d568bf99d394d19c08de7df56ea89a32daed
Vitor Lopes <vlopes@gmail.com>
2020-02-11 16:08:14 +0000

    add mobile 1 steps

--- src/static/js/js.js
+++ src/static/js/js.js
@@ -4,6 +4,10 @@ $(function () {
   });
 });
 
+$( ".logo" ).click(function() {
+  $( "#sidebar" ).toggle();
+});
+
 function debounce(fn, threshold) {
   var timeout;
   threshold = threshold || 600;
@@ -40,6 +44,19 @@ $('#search').keyup(debounce(function () {
   matchApp(app, searchValue)
 }, 1200));
 
-$( document ).ready(function() {
+$(document).ready(function () {
   app = $("main div .app");
+  if ('loading' in HTMLImageElement.prototype) {
+    const images = document.querySelectorAll("img.lazyload");
+    images.forEach(img => {
+      img.src = img.dataset.src;
+    });
+  } else {
+    // Dynamically import the LazySizes library
+    let script = document.createElement("script");
+    script.async = true;
+    script.src =
+      "https://cdnjs.cloudflare.com/ajax/libs/lazysizes/4.1.8/lazysizes.min.js";
+    document.body.appendChild(script);
+  }
 });
