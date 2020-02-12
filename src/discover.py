c54d3dc72bf1936f194ffd60f68e5e1ac69cc1f7
Vitor Lopes <vlopes@gmail.com>
2020-02-12 04:17:46 +0000

    add changes for wsgi

---
+++ src/discover.py
@@ -0,0 +1,6 @@
+from flask import Flask
+app = Flask(__name__)
+from routes import *
+
+if __name__ == "__main__":
+    app.run(port=8080, host="127.0.0.1", debug=True)
---
+++ src/wsgi.py
@@ -0,0 +1,4 @@
+from discover import app
+
+if __name__ == "__main__":
+    app.run()
