fe1f792c8c18ed880736e065f428d4d1ba02f1a6
papajoker <papajoke@manjaro.fr>
2020-02-26 07:51:04 +0100

    template filters
      create 2 filters
    
         * trunc_desc #11 : can pass "limit" to parameter  {{ description | trunc_desc(60) }} or not {{ description | trunc_desc }}
         * class_manjaro : can used for tag manjaro packages (icon.class or div.content ... ) class="xxx{{ pkg | class_manjaro }}yyy"

--- src/discover.py
+++ src/discover.py
@@ -2,6 +2,10 @@
 from flask import Flask
 app = Flask(__name__)
 from routes import *
+from templateutils import *
+
 
 if __name__ == "__main__":
+    app.jinja_env.filters['trunc_desc'] = truncate_desc
+    app.jinja_env.filters['class_manjaro'] = get_class_manjaro
     app.run(port=8000, host="127.0.0.1", debug=True)
