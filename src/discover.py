fb36cf6a170ad9193185e426717e037859f40cdc
patrick <papajoke@manjaro.fr>
2020-02-26 07:18:47 +0100

    trim templates
    remove empty line make by template conditions

--- src/discover.py
+++ src/discover.py
@@ -4,4 +4,6 @@ app = Flask(__name__)
 from routes import *
 
 if __name__ == "__main__":
+    app.jinja_env.trim_blocks = True
+    app.jinja_env.lstrip_blocks = True
     app.run(port=8000, host="127.0.0.1", debug=True)
