938ad3df1f03ea0aba1940efcaeda22807c8c2c5
Vitor Lopes <vlopes@gmail.com>
2020-03-02 23:23:16 +0000

    add flask cache

--- src/discover/__init__.py
+++ src/discover/__init__.py
@@ -1,6 +1,12 @@
 from flask import Flask
+from flask_caching import Cache
 from discover.templateUtils import truncate_description
 app = Flask(__name__)
+cache = Cache(app, config={
+    "CACHE_TYPE": "filesystem",
+    'CACHE_DIR': './data/cache',
+    "CACHE_DEFAULT_TIMEOUT": 3600
+})
 app.jinja_env.filters['truncate_description'] = truncate_description
 app.jinja_env.trim_blocks = True
 app.jinja_env.lstrip_blocks = True
--- src/discover/views.py
+++ src/discover/views.py
@@ -1,57 +1,68 @@
-from discover import app
+from discover import app, cache
 import discover.render as render
 
 
 @app.route("/applications/")
+@cache.cached(timeout=50)
 def applications():
     return render.pkgs_template(render.get_categories()[1].get("title"))
 
 
 @app.route("/applications/<application>")
+@cache.cached(timeout=40)
 def application(application):
     return render.search_package_template(application, "Application")
 
 
 @app.route("/packages/<package>")
+@cache.cached(timeout=40)
 def package(package):
     return render.search_package_template(package, "Package")
 
 
 @app.route("/snaps/<snap>")
+@cache.cached(timeout=40)
 def snap(snap):
     return render.search_package_template(snap, "Snap")
 
 
 @app.route("/flatpaks/<flatpak>")
+@cache.cached(timeout=40)
 def flatpak(flatpak):
     return render.search_package_template(flatpak, "Flatpak")
     
 
 @app.route("/packages/")
+@cache.cached(timeout=80)
 def packages():
     return render.pkgs_template(render.get_categories()[2].get("title"))
 
 
 @app.route("/snaps/")
+@cache.cached(timeout=50)
 def snaps():
     return render.external_repos_template(render.get_categories()[3].get("title"))
 
 
 @app.route("/flatpaks/")
+@cache.cached(timeout=50)
 def flatpaks():
     return render.external_repos_template(render.get_categories()[4].get("title"))
 
 
 @app.route("/")
+@cache.cached(timeout=40)
 def featured():
     return render.appstream_template(render.get_categories()[0].get("title"))
 
 
 @app.route("/<error404>/")
+@cache.cached(timeout=40)
 def error_404(error404):
     return render.template_404()
 
 
 @app.route('/sitemap.xml', methods=['GET'])
+@cache.cached(timeout=100)
 def sitemap():
     return render.sitemap_template()
