991c215411429a03c5182ecdcb3b94ec847d53d5
Vitor Lopes <vmnlop@gmail.com>
2022-01-08 20:12:48 +0000

    add preload/prefetch and drop jquery dependecy

--- src/discover/static/js/discover.js
+++ src/discover/static/js/discover.js
@@ -1,9 +1,3 @@
-$(function() {
-    $("form").submit(function() {
-        return false;
-    });
-});
-
 function debounce(fn, threshold) {
     var timeout;
     threshold = threshold || 600;
@@ -19,32 +13,35 @@ function debounce(fn, threshold) {
     };
 }
 
-function matchApp(app, searchValue) {
+function matchApp(searchValue) {
+    let package = document.querySelectorAll("main #search-items .app")
     if (searchValue == "") {
-        $(app).each(function() {
-            $(this).show()
+        package.forEach(function(el) {
+            el.style.display = "block"
         })
     } else {
-        $(app).hide()
-        $(app).each(function() {
-            if ($(this).find(".data-search").text().toLocaleLowerCase().includes(searchValue)) {
-                let image = $(this).find("img").attr('data-src')
-                $(this).find("img").attr('src', image);
-                $(this).show()
+        package.forEach(function (el) {
+            el.style.display = "none"
+            if (el.querySelector(".data-search").textContent.toLocaleLowerCase().includes(searchValue)) {
+                el.style.display = "block"
+                window.scrollTo(0, 0)
+                lazyLoad()
             }
         })
     }
 }
 
-$("#autocomplete-input").focus(function() {
-    $(".search-container").addClass("active")
+document.querySelector("#autocomplete-input").addEventListener('focus', function() {
+    document.querySelector(".search-container").classList.add("active")
 })
 
-$("#autocomplete-input").focusout(function () {
+document.querySelector("#autocomplete-input").addEventListener("focusout", function () {
     function is_open() {
-        const active = $(".autocomplete-content").css("display")
-        if (active == "none" && !$("#autocomplete-input").is(":focus")) {
-            $(".search-container").removeClass("active")
+        let search = document.querySelector(".search-container")
+        let results = document.querySelector(".autocomplete-content").style.display
+        const active = search.classList.contains("active")
+        if (!search.hasFocus && results != "block") {
+            document.querySelector(".search-container").classList.remove("active")
         } else {
             setTimeout(is_open, 20);
         }
@@ -52,32 +49,45 @@ $("#autocomplete-input").focusout(function () {
     is_open()
 })
 
-$("#clear-search").click(function() {
-    $("#autocomplete-input").val("")
-    $(".app").show()
+document.querySelector("#clear-search").addEventListener('click', function() {
+    document.querySelector("#autocomplete-input").value = ""
+    let app = document.querySelectorAll(".app")
+    app.forEach(function (el) {
+        el.style.display = "block"
+    });
     lazyLoad()
 })
 
-$('#autocomplete-input').keyup(debounce(function() {
-    $("body").addClass("wait");
-    let searchValue = $('#autocomplete-input').val().toLocaleLowerCase()
-    matchApp(app, searchValue)
-    setTimeout(function() {
-        $("body").removeClass("wait");
-    }, 300);
+document.querySelector('#autocomplete-input').addEventListener('keyup', debounce(function () {
+    let searchValue = this.value.toLocaleLowerCase()
+    matchApp(searchValue)
 }, 1200));
 
-$(document).ready(function() {
-    $('.sidenav').sidenav();
-    $('.materialboxed').materialbox();
-    let searchData = $('#search-items').attr("data-src")
-    if (searchData) {
-        let data = JSON.parse(searchData);
-        $('input.autocomplete').autocomplete({
+window.addEventListener('DOMContentLoaded', function() {
+    var sidenav = document.querySelector('.sidenav');
+    var sidenavInstances = M.Sidenav.init(sidenav);
+    var modals = document.querySelectorAll('.modal');
+    var modalInstances = M.Modal.init(modals);
+    var tooltips = document.querySelectorAll('.tooltipped');
+    var tooltipInstances = M.Tooltip.init(tooltips);
+    var materialboxed = document.querySelectorAll('.materialboxed');
+    var materialboxedInstances = M.Materialbox.init(materialboxed);
+    var carousel = document.querySelectorAll('.carousel');
+    var carouselInstances = M.Carousel.init(
+        carousel, {
+        fullWidth: true,
+        indicators: true
+    }
+    );
+    let searchData = document.querySelector('#search-items')
+    if (searchData != null) {
+        let data = JSON.parse(searchData.dataset.src);
+        var autocomplete = document.querySelector('.autocomplete');
+        var options = {
             limit: 100,
             data: data,
             onAutocomplete: function (val) {
-                var value = $('input.autocomplete').val();
+                var value = document.querySelector('input.autocomplete').value
                 for (key in data) {
                     if (value == key) {
                         if (location.pathname == "/snaps") {
@@ -92,37 +102,23 @@ $(document).ready(function() {
                     }
                 }
             },
-        });
+        }
+        var autocompleteInstances = M.Autocomplete.init(autocomplete, options);
     }
-    var modals = document.querySelectorAll('.modal');
-    var modalInstances = M.Modal.init(modals);
-    var tooltips = document.querySelectorAll('.tooltipped');
-    var tooltipInstances = M.Tooltip.init(tooltips);
-    var carousel = document.querySelectorAll('.carousel');
-    var carouselInstances = M.Carousel.init(
-        carousel, {
-        fullWidth: true,
-        indicators: true
-    }
-    );
-    app = $("#search-items .app");
     let lazyImages = [].slice.call(document.querySelectorAll("img.lazyload"));
     let active = false;
 
     lazyLoad = function() {
         if (active === false) {
             active = true;
-
             setTimeout(function() {
                 lazyImages.forEach(function(lazyImage) {
                     if ((lazyImage.getBoundingClientRect().top <= window.innerHeight && lazyImage.getBoundingClientRect().bottom >= 0) && getComputedStyle(lazyImage).display !== "none") {
                         lazyImage.src = lazyImage.dataset.src;
                         //lazyImage.classList.remove("lazyload");
-
                         lazyImages = lazyImages.filter(function(image) {
                             return image !== lazyImage;
                         });
-
                         if (lazyImages.length === 0) {
                             document.removeEventListener("scroll", lazyLoad);
                             window.removeEventListener("resize", lazyLoad);
@@ -134,13 +130,8 @@ $(document).ready(function() {
             }, 200);
         }
     };
-
     document.addEventListener("scroll", lazyLoad);
     window.addEventListener("resize", lazyLoad);
     window.addEventListener("orientationchange", lazyLoad);
-
-    function forceImageLoad() {
-        $(window).scrollTop($(window).scrollTop() + 1)
-    }
-    window.addEventListener('pageshow', forceImageLoad());
-});
+    window.addEventListener('pageshow', lazyLoad);
+})
