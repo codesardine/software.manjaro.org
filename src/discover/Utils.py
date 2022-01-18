a8685b6ef5aae78d73bb52d532b9bb077a4373cf
Vitor Lopes <vmnlop@gmail.com>
2022-01-18 15:06:50 +0000

    revert a46bb328b3dc0090d7c41d374a4cde6504cce7df

--- src/discover/Utils.py
+++ src/discover/Utils.py
@@ -4,6 +4,6 @@ from discover.database import Database
 
 def update_system():
     subprocess.run(
-       "pacman-mirrors -f 5 && pacman -Syy --noconfirm archlinux-appstream-data && systemctl restart discover", shell=True
+       "pacman-mirrors -f 5 && pacman -Syy --noconfirm archlinux-appstream-data", shell=True
       )
     Database().reload_tables()
