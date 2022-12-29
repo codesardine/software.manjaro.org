5a114e720ba4920ad151e84b0cf81926489c3758
Vitor Lopes <vmnlop@gmail.com>
2022-12-29 19:05:07 +0000

    remove pacman-mirrors

--- src/discover/Utils.py
+++ src/discover/Utils.py
@@ -5,6 +5,6 @@ from Manjaro.SDK import Utils
 @Utils._async
 def update_system():
     subprocess.run(
-       "pacman-mirrors -f 5 && pacman -Syy --noconfirm archlinux-appstream-data", shell=True
+       "pacman -Syy --noconfirm archlinux-appstream-data", shell=True
       )
     Database().reload_tables()
